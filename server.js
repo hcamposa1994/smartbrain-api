fde604e1311520c9a3c0578b8d66750139bca0bd
Herbert Campos <wohoforever@yahoo.com>
2020-12-25 21:44:22 -0800

    edited the register route to also to insert a bcrypt hash and email unto the login table, and edited the signin route to be able to dehash password to compare that to inputted credentials

--- server.js
+++ server.js
@@ -20,62 +20,61 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: "123",
-      name: "John",
-      password: "cookies",
-      email: "john@gmail.com",
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: "124",
-      name: "Sally",
-      email: "sally@gmail.com",
-      password: "bananas",
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-  login: [
-    {
-      id: "987",
-      hash: "",
-      email: "john@gmail.com",
-    },
-  ],
-};
-
 app.get("/", (req, res) => {
-  res.send(database.users);
+  res.send("success");
 });
 
 app.post("/signin", (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json("error logging in");
-  }
+  db.select("email", "hash")
+    .where("email", "=", req.body.email)
+    .from("login")
+    .then((data) => {
+      bcrypt.compare(req.body.password, data[0].hash, function (err, response) {
+        if (response) {
+          return db
+            .select()
+            .from("users")
+            .where("email", "=", req.body.email)
+            .then((user) => {
+              res.json(user[0]);
+            })
+            .catch((err) => res.status(400).json("unable to get user"));
+        } else {
+          res.status(400).json("wrong credentials");
+        }
+      });
+    })
+    .catch((err) => res.status(400).json("wrong credentials"));
 });
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  db("users")
-    .returning("*")
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
-    })
-    .catch((err) => res.status(400).json("unable to register"));
+
+  bcrypt.hash(password, 10, function (err, hash) {
+    db.transaction((trx) => {
+      trx
+        .insert({
+          hash: hash,
+          email: email,
+        })
+        .into("login")
+        .returning("email")
+        .then((loginEmail) => {
+          return trx("users")
+            .returning("*")
+            .insert({
+              email: loginEmail[0],
+              name: name,
+              joined: new Date(),
+            })
+            .then((user) => {
+              res.json(user[0]);
+            });
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    }).catch((err) => res.status(400).json("unable to register"));
+  });
 });
 
 app.get("/profile/:id", (req, res) => {
