a1a79d89ce512e274c40a1313b623ee02c1b147c
Herbert Campos <wohoforever@yahoo.com>
2020-12-29 15:10:46 -0800

    edited the port to use the defalut port heroku uses, otherwise default back to 3000

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const db = knex({
 });
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
@@ -34,6 +35,6 @@ app.get("/profile/:id", (req, res) => handleProfileGet(req, res, db));
 app.put("/image", (req, res) => handleImage(req, res, db));
 app.post("/imageurl", (req, res) => handleApiCall(req, res));
 
-app.listen(process.env.DB_PORT, () => {
-  console.log(`app is running on port ${process.env.DB_PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 });
